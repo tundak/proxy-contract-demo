52e7c83e45ec428485fad963fe4375b590957660
tundak <sonu5650@gmail.com>
2021-09-22 18:35:33 +0530

    kovan deployment test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,25 @@
 require('@nomiclabs/hardhat-ethers');
 require('@openzeppelin/hardhat-upgrades');
+require("@nomiclabs/hardhat-waffle");
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "rVOVNhQIVi9jSsItwblvgvieCIIbzrlT";
+
+// Replace this private key with your Kovan account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const KOVAN_PRIVATE_KEY = "ad0bca56a437c4961f56499f54f48a8d7cf1128e0201abcc7115994042cb6713";
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    kovan: {
+      url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${KOVAN_PRIVATE_KEY}`],
+    },
+  },
 };
--- scripts/2_deploy.js
+++ scripts/2_deploy.js
@@ -3,7 +3,7 @@ const { ethers, upgrades } = require('hardhat');
 async function main() {
   // We get the contract to deploy
   const ReferralV2 = await ethers.getContractFactory('ReferralV2');
-  const ref =   await upgrades.upgradeProxy('0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0', ReferralV2);
+  const ref =   await upgrades.upgradeProxy('0xB7bb8f008f304d4CfC9ED7425B4C518BD892B91a', ReferralV2);
 
   console.log("ReferralV2 deployed to:", ref.address);
 }
